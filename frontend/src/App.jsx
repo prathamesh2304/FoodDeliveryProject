856caec2dba04cf55ec98413195bf1314fceec36
prathamesh2304 <prathameshthakur.engg@gmail.com>
2025-12-12 18:07:30 +0530

    Assets n basic template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import NavBar from './components/Navbar/NavBar.jsx'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <NavBar />
+      </div>
   )
 }
 
