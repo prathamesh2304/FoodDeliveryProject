d81164257d3d810a2193890ae20528b94e805d97
prathamesh2304 <prathameshthakur.engg@gmail.com>
2025-12-14 13:06:47 +0530

    Navbar n Routes Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 // App.jsx
 import React from 'react'
 import NavBar from './components/Navbar/NavBar.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 
 
 const App = () => {
   return (
     <div className='app'>
       <NavBar />
-      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
+    </div>
   )
 }
 
