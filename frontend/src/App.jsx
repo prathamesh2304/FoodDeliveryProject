7f1aa42710ee23a0262f1bdbe40af2068e1cb2a9
prathamesh2304 <prathameshthakur.engg@gmail.com>
2025-12-16 12:25:03 +0530

    Footer Is Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,21 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
-
+import Footer from './components/Footer/Footer.jsx'
 
 const App = () => {
   return (
-    <div className='app'>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/order' element={<PlaceOrder />} />
-      </Routes>
-    </div>
+    <>
+      <div className='app'>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
