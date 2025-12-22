f15ae83b70083e53bcfcc37aed5df50ba403caf4
prathamesh2304 <prathameshthakur.engg@gmail.com>
2025-12-22 20:53:31 +0530

    Cart Page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,15 @@
+//Cart.jsx
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoredContext'
 const Cart = () => {
 
-  const {cartItems, food_list, removeFromCart} = useContext(StoreContext);
+  const {cartItem, food_list, removeFromCart} = useContext(StoreContext);
 
+console.log("CART ITEM STATE:", cartItem);
 
   return (
+    
     <div className='cart'>
       <div className="cart-items">
         <div className="cart-items-title">
@@ -20,16 +23,19 @@ const Cart = () => {
         <br />
         <hr />
         {food_list.map((item,index)=>{
-          if(cartItems[item._id]>0)
+          if(cartItem[item._id]>0)
           {
             return (
+              <div>
               <div className="cart-items-title cart-items-item">
                 <img src={item.image} alt="" />
                 <p>{item.name}</p>
                 <p>{item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>{item.price*cartItems[item._id]}</p>
+                <p>{cartItem[item._id]}</p>
+                <p>{item.price*cartItem[item._id]}</p>
                 <p>x</p>
+              </div>
+              <hr />
               </div>
             )
           }
